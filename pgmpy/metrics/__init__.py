115193b1cb1b1e012e64f5560c86596664c9a06b
Jihye Sofia Seo <jihyeseo@gmail.com>
2025-02-08 15:16:37 +0100

    Implement SID with is_valid_adjustment_set (almost working)

--- pgmpy/metrics/__init__.py
+++ pgmpy/metrics/__init__.py
@@ -1,18 +1,22 @@
 from .bn_inference import BayesianModelProbability
 from .metrics import (
     SHD,
+    SID,
     correlation_score,
     fisher_c,
     implied_cis,
     log_likelihood_score,
     structure_score,
+    sid_matrix,
 )
 
 __all__ = [
     "correlation_score",
     "log_likelihood_score",
+    "sid_matrix",
     "structure_score",
     "implied_cis",
     "fisher_c",
     "SHD",
+    "SID",
 ]
